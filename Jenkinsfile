f73d17d639799988c9086810757c1a947add3449
Anusha1996-stack <f.anusha@globaledgesoft.com>
2021-03-18 19:11:43 +0530

    changes

---
+++ Jenkinsfile
@@ -0,0 +1,11 @@
+@Library('msbuild@master')_
+
+node {
+
+	stage('msbuild running')
+	{
+		println("msbuild file calling")
+		msbuild()
+		println("msbuild file executed")
+	}
+}
---
+++ vars/msbuild.groovy
@@ -0,0 +1,25 @@
+def call (String buildParams, String projectname, String buildDir, String Platform)
+{
+sh '''
+dir(buildDir)
+{
+def W = ["WES_UTILS"];
+
+      println(W.contains(projectname));
+
+	version(projectname)
+
+if( projectname.take(W) == "WES_UTILS")
+{
+	bat '''
+        msbuild /t:clean/property:configuration=Release
+'''
+}
+
+bat '''
+msbuild /t:build/property:configuration=Release "${buildParams}"
+'''
+'''
+}
+
+}
